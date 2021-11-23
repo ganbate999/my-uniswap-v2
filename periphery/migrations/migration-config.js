54a4bf0b656b660dd6ecdb214c0814887f8b02d1
Hosokawa Zen <56865797+hosokawa-zen@users.noreply.github.com>
2021-11-23 22:12:38 +0800

    init commit

---
+++ periphery/migrations/migration-config.js
@@ -0,0 +1,16 @@
+const config = {
+    'testnet': {
+        factoryAddress: '0x',
+        WETHAddress: '0x'
+    },
+    'development': {
+        factoryAddress: '0xef21b3754fa928b4B251EE4B12C7e4D32B710C1C',
+        WETHAddress: '0x'
+    },
+    'bsc': {
+        factoryAddress: '0x',
+        WETHAddress: '0x'
+    }
+}
+
+exports.config = config;
